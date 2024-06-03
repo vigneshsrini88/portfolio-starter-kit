6d9960a8821a43276bc572f03b0bb43db17dc66a
vigneshsrini88 <vigneshsrini.88@gmail.com>
2024-06-03 15:58:39 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,10 @@ export default function Page() {
       <h2>About Me</h2>
       <p className="mb-4">
         {`Leadership and Project Management Expertise
-Led and mentored a team of skilled Technical Writers, fostering growth and delivering high-quality documentation. Successfully managed projects and collaborated with cross-functional teams, demonstrating expertise in project management, people management, and Agile methodologies. With a keen attention to detail, I ensure accuracy, clarity, and adherence to industry standards in all deliverables.
-
-Specialized Industry Knowledge and Technical Proficiency
-Specialize in creating user-friendly and SEO-optimized documentation for Aerospace, Healthcare, HVAC, and Telecommunications industries. Proficient in tools and technologies relevant to Technical Writing, including CMS (Content Management Systems), style guides, and documentation workflows. My approach includes implementing content strategy and streamlining processes for efficient content reuse and knowledge management.
-
-Innovative Solutions and Advanced Techniques
-Pioneered innovative approaches such as Augmented Reality and Generative chatbot integration in Technical Publications. My work emphasizes not only traditional documentation excellence but also cutting-edge methods to enhance user experience and accessibility. These initiatives have significantly improved the effectiveness and reach of the documentation produced.`}
-      </p>
+Led and mentored a team of skilled Technical Writers, fostering growth and delivering high-quality documentation. Successfully managed projects and collaborated with cross-functional teams, demonstrating expertise in project management, people management, and Agile methodologies. With a keen attention to detail, I ensure accuracy, clarity, and adherence to industry standards in all deliverables.`}
+      </p><p className="mb-4">
+        {`Specialized Industry Knowledge and Technical Proficiency
+Specialize in creating user-friendly and SEO-optimized documentation for Aerospace, Healthcare, HVAC, and Telecommunications industries. Proficient in tools and technologies relevant to Technical Writing, including CMS (Content Management Systems), style guides, and documentation workflows. My approach includes implementing content strategy and streamlining processes for efficient content reuse and knowledge management.`}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
